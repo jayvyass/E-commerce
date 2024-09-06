96fe51d4d9dc2bad796637a5f3acfc1cdab133f7
jayvyass <jayvyas08@yahoo.com>
2024-09-06 13:43:36 +0530

    first commit

--- myproject/settings.py
+++ myproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0i&c30c9@4$3!sc@!p@a1@f%j^rqac!#6ckly!z-@%&7a@9!!^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -131,6 +131,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = '/images/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
@@ -170,4 +171,3 @@ REST_FRAMEWORK = {
     ],
 }
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
--- myproject/urls.py
+++ myproject/urls.py
@@ -25,4 +25,4 @@ urlpatterns = [
     path('', include('myapp.urls')),
     path("__reload__/", include("django_browser_reload.urls")),
     
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
